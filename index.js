bc237af5b6c63a5a62afe58bfb0c1510a2766b7d
John Lombardo <john@programbo.com>
2015-11-25 23:54:23 +0800

    Remove unused modules

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const extend = require('lodash/object/extend');
 const auth = require('./lib/auth');
 const github = require('./lib/github');
-const print = require('./lib/print');
 
 let options = {
   onComplete: (diffs) => {
