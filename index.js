ce7d05953f9264e7a5d3caec53cd637d365cd438
John Lombardo <john@programbo.com>
2015-11-26 11:55:00 +0800

    Remove the literal token from auth.js

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const auth = require('./lib/auth');
 const github = require('./lib/github');
 
 module.exports = function (options) {
-  return github.getOpenPRs(auth, options.limit, options.debug);
+  return auth('token').then((credentials) => {
+    return github.getOpenPRs(credentials, options.limit, options.debug);
+  });
 };
