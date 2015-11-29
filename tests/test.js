b0e7047b17ae60e9c1edba2eb74bc71158d81451
John Lombardo <john@programbo.com>
2015-11-29 13:19:11 +0800

    Move tests into their own directory

---
+++ tests/test.js
@@ -0,0 +1,52 @@
+"use strict";
+const expect = require("chai").expect;
+
+describe("PRInspector", () => {
+  describe("print module", () => {
+    let stdoutWrite;
+    let log;
+    before(() => {
+      stdoutWrite = process.stdout.write;
+      process.stdout.write = (message) => {
+        log.push(message);
+      };
+    });
+    beforeEach(() => log = []);
+    afterEach(() => log = []);
+    after(() => process.stdout.write = stdoutWrite);
+    const print = require("../lib/print");
+    it("Should write to stdout", () => {
+      print("Hello world", true);
+      expect(log.slice(2).join("")).to.eql("Hello world\n");
+    });
+    it("should obey the conditional flag", () => {
+      print("Hello world", false);
+      expect(log.length).to.eql(0);
+    });
+    it("should concatenate an array", () => {
+      print(["Hello", " ", "world"], true);
+      expect(log.slice(-1).toString()).to.eql("Hello world\n");
+    });
+  });
+
+  describe("integration", () => {
+    let program;
+    before(() => {
+      const child_process = require("child_process");
+      program = child_process.spawn("./prinspector.js", ["-l", 2]);
+    });
+    it("should return an array", function (done) {
+      this.timeout(0);
+      let output = "";
+      program.stdout.on("data", (data) => {
+        output += data.toString();
+      });
+      program.on("exit", (exitCode) => {
+        expect(exitCode).to.eql(0);
+        const response = JSON.parse(output);
+        expect(response).to.be.an.instanceof(Array);
+        done();
+      });
+    });
+  });
+});
